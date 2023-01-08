c0cbca545b55b43a18ef67d453cc03a22a91f1a2
Eslam Alaa <zoro12zx@gmail.com>
2023-01-08 11:21:53 +0200

    EslamAlaa: Create chat list.

--- lib/widgits/chat_list.dart
+++ lib/widgits/chat_list.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chatgpt_clone/widgits/chat_message.dart';
 
 class ChatList extends StatelessWidget {
-  const ChatList({Key? key}) : super(key: key);
+  final List<ChatMessage> messages;
+  final ScrollController scrollController;
+  const ChatList({Key? key, required this.messages, required this.scrollController}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(
+      itemCount: messages.length,
+      controller: scrollController,
+      itemBuilder: (context, index) {
+        return ChatMessage();
+      },
+    );
   }
 }
---
+++ lib/widgits/chat_message.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChatMessage extends StatelessWidget {
+  const ChatMessage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
